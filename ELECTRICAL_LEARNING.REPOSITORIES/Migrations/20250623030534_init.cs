8ef81a38d7c1bfa56129d304a69429023bd74cb2
Quyen-Peter <quyentrinh040@gmail.com>
2025-06-23 10:21:27 +0700

    new commit

---
+++ ELECTRICAL_LEARNING.REPOSITORIES/Migrations/20250623030534_init.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ElectricalLearning.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grade",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grade", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AIRequest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AIRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AIRequest_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chapter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GradeId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chapter", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chapter_Grade_GradeId",
+                        column: x => x.GradeId,
+                        principalTable: "Grade",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lesson",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChapterId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lesson", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lesson_Chapter_ChapterId",
+                        column: x => x.ChapterId,
+                        principalTable: "Chapter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CircuitModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    JsonData = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    LessonId = table.Column<int>(type: "int", nullable: false),
+                    ChapterId = table.Column<int>(type: "int", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CircuitModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CircuitModel_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CircuitModel_Chapter_ChapterId",
+                        column: x => x.ChapterId,
+                        principalTable: "Chapter",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CircuitModel_Lesson_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lesson",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Exercise",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LessonId = table.Column<int>(type: "int", nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercise", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Exercise_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Exercise_Lesson_LessonId",
+                        column: x => x.LessonId,
+                        principalTable: "Lesson",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Formula",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Expression = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CircuitModelId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Formula", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Formula_CircuitModel_CircuitModelId",
+                        column: x => x.CircuitModelId,
+                        principalTable: "CircuitModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Submission",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    AccountId = table.Column<int>(type: "int", nullable: false),
+                    ExerciseId = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Submission", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Submission_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Submission_Exercise_ExerciseId",
+                        column: x => x.ExerciseId,
+                        principalTable: "Exercise",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Accounts",
+                columns: new[] { "Id", "CreatedAt", "Email", "FullName", "IsDeleted", "PasswordHash", "Role", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 471, DateTimeKind.Unspecified).AddTicks(6571), new TimeSpan(0, 0, 0, 0, 0)), "teacher@gmail.com", "Nguyễn Văn A", false, "$2a$11$1plzKL4WnDiQQ3hIR5/Xa.KHq2BM3coLkYhh/JgY8xgL/d7R.e1oO", "Teacher", new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { 2, new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 609, DateTimeKind.Unspecified).AddTicks(3992), new TimeSpan(0, 0, 0, 0, 0)), "student@gmail.com", "Trần Thị B", false, "$2a$11$7.F7A4vVpqnM1EtrdCf9EODC17SVV/auqZw307cuOuSkAtkYLdgpO", "Student", new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) },
+                    { 3, new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 745, DateTimeKind.Unspecified).AddTicks(9750), new TimeSpan(0, 0, 0, 0, 0)), "admin@gmail.com", "Lê Văn C", false, "$2a$11$U33PupEzmnLMpPAByaBFjeGFri91b345iCWksFcHYPIzka3r/2gP6", "Admin", new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Grade",
+                columns: new[] { "Id", "IsDeleted", "Name" },
+                values: new object[,]
+                {
+                    { 1, false, 6 },
+                    { 2, false, 7 },
+                    { 3, false, 8 },
+                    { 4, false, 9 },
+                    { 5, false, 10 },
+                    { 6, false, 11 },
+                    { 7, false, 12 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AIRequest",
+                columns: new[] { "Id", "AccountId", "CreatedAt", "ImageUrl", "IsDeleted", "Status", "UpdatedAt" },
+                values: new object[] { 1, 2, new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 746, DateTimeKind.Unspecified).AddTicks(3364), new TimeSpan(0, 0, 0, 0, 0)), "https://example.com/image1.png", false, "Pending", new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.InsertData(
+                table: "Chapter",
+                columns: new[] { "Id", "GradeId", "IsDeleted", "Name" },
+                values: new object[] { 1, 1, false, "Điện học cơ bản" });
+
+            migrationBuilder.InsertData(
+                table: "Lesson",
+                columns: new[] { "Id", "ChapterId", "IsDeleted", "Title" },
+                values: new object[] { 1, 1, false, "Định luật Ôm" });
+
+            migrationBuilder.InsertData(
+                table: "CircuitModel",
+                columns: new[] { "Id", "AccountId", "ChapterId", "CreatedAt", "IsDeleted", "JsonData", "LessonId", "Name", "UpdatedAt" },
+                values: new object[] { 1, 1, null, new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 746, DateTimeKind.Unspecified).AddTicks(5974), new TimeSpan(0, 0, 0, 0, 0)), false, "{\"resistor\": 100, \"voltage\": 5}", 1, "Mạch mẫu 1", new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.InsertData(
+                table: "Exercise",
+                columns: new[] { "Id", "AccountId", "IsDeleted", "LessonId", "Title" },
+                values: new object[] { 1, null, false, 1, "Khi đặt vào hai đầu dây dẫn một hiệu điện thế 12V thì cường độ dòng điện chạy qua nó là 0,5A.Tính điện trở? " });
+
+            migrationBuilder.InsertData(
+                table: "Formula",
+                columns: new[] { "Id", "CircuitModelId", "Description", "Expression", "IsDeleted", "Name" },
+                values: new object[] { 1, 1, "Cường độ dòng điện chạy qua dây dẫn tỉ lệ thuận với hiệu điện thế đặt vào hai đầu dây và tỉ lệ nghịch với điện trở của dây.", "I = U / R", false, "Định luật Ôm" });
+
+            migrationBuilder.InsertData(
+                table: "Submission",
+                columns: new[] { "Id", "AccountId", "Answer", "CreatedAt", "ExerciseId", "IsDeleted", "UpdatedAt" },
+                values: new object[] { 1, 2, "R = 6 ohm", new DateTimeOffset(new DateTime(2025, 6, 23, 3, 5, 33, 747, DateTimeKind.Unspecified).AddTicks(361), new TimeSpan(0, 0, 0, 0, 0)), 1, false, new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AIRequest_AccountId",
+                table: "AIRequest",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chapter_GradeId",
+                table: "Chapter",
+                column: "GradeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CircuitModel_AccountId",
+                table: "CircuitModel",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CircuitModel_ChapterId",
+                table: "CircuitModel",
+                column: "ChapterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CircuitModel_LessonId",
+                table: "CircuitModel",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercise_AccountId",
+                table: "Exercise",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Exercise_LessonId",
+                table: "Exercise",
+                column: "LessonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Formula_CircuitModelId",
+                table: "Formula",
+                column: "CircuitModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lesson_ChapterId",
+                table: "Lesson",
+                column: "ChapterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Submission_AccountId",
+                table: "Submission",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Submission_ExerciseId",
+                table: "Submission",
+                column: "ExerciseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AIRequest");
+
+            migrationBuilder.DropTable(
+                name: "Formula");
+
+            migrationBuilder.DropTable(
+                name: "Submission");
+
+            migrationBuilder.DropTable(
+                name: "CircuitModel");
+
+            migrationBuilder.DropTable(
+                name: "Exercise");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "Lesson");
+
+            migrationBuilder.DropTable(
+                name: "Chapter");
+
+            migrationBuilder.DropTable(
+                name: "Grade");
+        }
+    }
+}
